3915e3ca2a3d7a1eb342a120d4e9ae25b37144b4
Frostquill <ruiz82494@gmail.com>
2021-05-10 13:30:19 -0700

    Timer added

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,1 +1,29 @@
-alert("test window")
+var start = document.getElementById("Start");
+var time = document.getElementById("Time");
+
+var questions = [
+    {q: 'Javascript '}
+]
+
+
+
+function countdown() {
+    var timeLeft = 5;
+
+    var quizTimer = setInterval(function() {
+        if (timeLeft > 0) {
+            time.textContent = 'Time: ' + timeLeft;
+            timeLeft--;
+        } 
+        else if (timeLeft === 0) {
+            time.textContent = "your out of time!";
+            clearInterval(quizTimer);
+            
+        }
+        
+       
+    }, 1000);
+    
+}
+
+start.onclick = countdown;
